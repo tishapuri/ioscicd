27ab5f99bc3773d7d8464d15ffe9ca555f54df54
tishapuri <8tisha.puri@gmail.com>
2025-03-21 18:49:04 +0530

    Update ios_demoUITests.swift

--- ios demoUITests/ios_demoUITests.swift
+++ ios demoUITests/ios_demoUITests.swift
@@ -24,8 +24,8 @@ final class ios_demoUITests: XCTestCase {
 
     func testExample() throws {
         // UI tests must launch the application that they test.
-        //let app = XCUIApplication()
-        //app.launch()
+        let app = XCUIApplication()
+        app.launch()
 
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
@@ -34,7 +34,7 @@ final class ios_demoUITests: XCTestCase {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
             //measure(metrics: [XCTApplicationLaunchMetric()]) {
-                //XCUIApplication().launch()
+                XCUIApplication().launch()
             }
         }
     }
